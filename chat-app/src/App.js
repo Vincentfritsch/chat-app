264cf122294b8731e9e08fb4ccfa451d0a9e3c71
Vincent <v.fritsch1981@gmail.com>
2020-07-18 20:15:19 +0200

    Quest_2 done

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -3,7 +3,23 @@ import Contact from "./components/Contact";
 
 function App() {
   return (
-   <Contact/>
+  <>
+       <Contact
+          avatar = "https://randomuser.me/api/portraits/women/47.jpg"
+          name = "Beverley Barrett"
+          online = {true}
+          />,
+        <Contact
+        avatar = "https://randomuser.me/api/portraits/women/46.jpg"
+        name = "April Craig"
+        online = {false}
+        />,
+        <Contact
+        avatar = "https://randomuser.me/api/portraits/women/48.jpg"
+        name = "Letitia Gibson"
+        online = {true}
+        />
+    </>
   );
 }
 
--- chat-app/src/components/Contact.js
+++ chat-app/src/components/Contact.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Contact() {
+function Contact(props) {
     return (
-        <div className="Contact">
-            <img className="avatar" src="https://randomuser.me/api/portraits/women/47.jpg" alt="Woman"/>
-            <div>
-                <p className="name">Beverley Barrett</p>
-                <div className="status">
-                    <div className="status-online">
+           <div className="Contact">
+                <img className="avatar" src={props.avatar}
+                     alt="Woman"/>
+                <div>
+                    <p className="name">{props.name}</p>
+                    <div className="status">
+                        <div className={props.online ? 'status-online' : 'status-offline'}>
+                        </div>
+                        <div className="status-text">{props.online ? 'online' : 'offline'}</div>
                     </div>
-                    <div className="status-text">online</div>
                 </div>
             </div>
 
-        </div>
     )
 }
+
+Contact.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    online: PropTypes.bool.isRequired,
+};
 
 export default Contact;
